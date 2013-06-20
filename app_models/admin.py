590db962c124b7a08973fec32859e0a20857b0b9
Ken Schwencke <schwank@gmail.com>
2013-06-19 23:48:26 -0700

    osmgeoadmin

--- app_models/admin.py
+++ app_models/admin.py
@@ -1,7 +1,7 @@
 from django.contrib.gis import admin
 from models import BikePath
 
-class BikePathAdmin(admin.ModelAdmin):
+class BikePathAdmin(admin.OSMGeoAdmin):
     list_display = ("user", "date_time")
     list_filter = ('user',)
 admin.site.register(BikePath, BikePathAdmin)
