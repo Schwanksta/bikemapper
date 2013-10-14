a7da3a3ab818fbe293b99a39a9934d613d5c734d
Ken Schwencke <schwank@gmail.com>
2013-10-14 08:35:05 -0700

    fiddle with the admin and change json form type

--- app_models/admin.py
+++ app_models/admin.py
@@ -2,6 +2,6 @@ from django.contrib.gis import admin
 from models import BikePath
 
 class BikePathAdmin(admin.OSMGeoAdmin):
-    list_display = ("user", "date_time")
-    list_filter = ('user',)
+    list_display = ('user', 'approved', 'rating', 'date_time',)
+    list_filter = ('user', 'approved', 'rating')
 admin.site.register(BikePath, BikePathAdmin)
